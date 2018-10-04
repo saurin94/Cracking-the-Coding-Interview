5d94f70785249556739be4e0781645f01acaf6f3
Saurin Shah <shah.saurin@husky.neu.edu>
2018-10-04 18:44:21 -0400

    added comments

--- Trees and Graphs/CheckBalanced.py
+++ Trees and Graphs/CheckBalanced.py
@@ -1,3 +1,23 @@
+'''
+Check Balanced: Implement a function to check if a binary tree is balanced.
+For the purposes of this question, a balanced tree is defined to be a tree such that
+the heights of the two subtrees of any node never differ by more than one.
+
+'''
+
+
+
+
+'''To find if a tree of size n nodes is balanced:
+
+1- Solve the same problem for the right subtree
+
+2- Solve the same problem for the left subtree
+
+3- Get the heights of the left and right subtrees
+'''
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -37,3 +57,7 @@ root.left.left.left = Node(7)
 
 
 print "Is the tree balanced ? \nAnswer is : ", is_balanced(root)
+
+
+#Time complexity :
+#T(n) = 2 * T(n / 2) + o(n) => O(n)
